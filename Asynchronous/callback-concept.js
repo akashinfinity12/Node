9485f8051d988256902f4f1cc0540bb8d8792647
akash <akashudsouza12@gmail.com>
2023-01-26 17:16:01 +0530

    callback concept

--- Asynchronous/callback-concept.js
+++ Asynchronous/callback-concept.js
@@ -1,3 +1,36 @@
+// Callback
+
+console.log("Before");
+getUser(1, (item) => {
+  console.log(item);
+  getRepositories(item.name, (e) => {
+    console.log("Repositories: " + e);
+  });
+});
+console.log("After");
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log(`Reading user ${id} data from the database..`);
+    const userInformation = {
+      name: "Akash",
+      interests: ["Computer Science, Math"],
+    };
+    callback(userInformation);
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log(`Fetching ${username}'s repository list..`);
+    callback(["Java", "Node", "YUGMA-B2C-APP"]);
+  }, 2000);
+}
+
+/*
+
+This code snippet is very important in understanding async concept and why we require callbacks, async-await and promises
+
 console.log("Before");
 const user = getUser(1);
 console.log(user);
@@ -13,3 +46,5 @@ function getUser(id) {
     return userInformation;
   }, 2000);
 }
+
+*/
